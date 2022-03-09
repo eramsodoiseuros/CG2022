2581d929a254d72673ca2ef17afb6a8b4f6e3935
diogo-Rebelo01 <62115111+diogo-Rebelo01@users.noreply.github.com>
2022-03-09 14:43:05 +0000

    updated sphere.h and added sphere.cpp

--- generator/sphere.h
+++ generator/sphere.h
@@ -13,12 +13,21 @@ class Sphere {
         float raio;
         int stacks;
         int slices;
+        Point_3D center;
+
+        std::vector<Point_3D> spherePoints;     // Pontos da esfera
+        std::vector<short> sphereIndexes;           // índices para construção
 
     public:
         Sphere(float r, int s1, int s2) {
+            this->center = Point_3D(0.0f,0.0f,0.0f);
             this->raio = r;
             this->stacks = s1;
             this->slices = s2;
+
+            spherePoints = calculateSphere(center,raio,stacks,slices);
+            sphereIndexes = calculateIndexes();
+
         }
 
         /**
@@ -43,7 +52,32 @@ class Sphere {
         *	Retorna o Modelo da Esfera
         *	@param output_file ficheiro resultante
         */
-        void toFile(char* output_file) const;
+        void toFile(const char* output_file);
+
+
+        /**
+         * @brief Calcula os pontos da esfera
+         * @return vetor com os pontos
+         */
+        static std::vector<Point_3D> calculateSphere(Point_3D center, float height, int slices, int stacks);
+        
+        /**
+         * @brief Calcula os indices para construção
+         * @param points Pontos da esfera
+         * @return vetor com os indices
+         */
+        std::vector<short> calculateIndexes();
+        
+        /**
+         * @brief Get the Sphere Points object
+         * @return Pontos da esfera
+         */
+        std::vector<Point_3D> getSpherePoints();
+        /**
+         * @brief Get the Sphere Points Indexes object
+         * @return Indexes
+         */
+        std::vector<short> getSpherePointsIndexes();
 };
 
 

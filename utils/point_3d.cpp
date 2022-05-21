cff78505811c1bb8b86bb3beba8e8d7e7781beab
Francisco Peixoto <thtr.irubra@gmail.com>
2022-05-21 16:16:57 +0100

    typo fix

--- utils/point_3d.cpp
+++ utils/point_3d.cpp
@@ -28,7 +28,7 @@ Point_3D crossProduct(Point_3D p) {
 void normal_vetor() {
 
     float length = sqrt(pow(this->x, 2) + pow(this->y, 2) + pow(this->z, 2));
-    
+
     this->setX(this->getX() / length);
     this->setY(this->getY() / length);
     this->setZ(this->getZ() / length);
@@ -38,7 +38,7 @@ Point_3D calcular_normal(Point_3D vetorA, Point_3D vetorB, Point_3D vetorC){
     Point_3D x = {(b[0]-a[0], b[1]-a[1], b[2]-a[2])};
     Point_3D y = {(c[0]-a[0], c[1]-a[1], c[2]-a[2])};
 
-    Point_3D* r = x.crossProduct(y);
+    Point_3D r = x.crossProduct(y);
 
     return r;
 }
